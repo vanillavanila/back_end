a48e304e4b3ebbc2d4bc3db2f5cb6e1539caa4cc
12rekayasa2-rpl <12rekayasa2@gmail.com>
2026-05-22 21:19:00 +0800

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,20 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::updateOrCreate(
-            [
-                'email' => 'adminsmk@gmail.com',
-            ],
+            ['email' => 'adminsmk@gmail.com'],
             [
                 'name' => 'Super Admin',
-                'password' => Hash::make('123'),
                 'role' => 'super_admin',
+                'password' => Hash::make('password'),
             ]
         );
     }
