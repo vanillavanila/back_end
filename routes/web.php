43866e02d761f82cd2bd701b5f3923f1587ea980
12rekayasa2-rpl <12rekayasa2@gmail.com>
2026-05-26 15:50:41 +0800

    debug auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,4 +11,18 @@ Route::get('/', function () {
 Route::get('/clear', function () {
     Artisan::call('optimize:clear');
     return 'cache cleared';
+});
+
+Route::get('/test-login', function () {
+
+    $credentials = [
+        'email' => 'adminsmk@gmail.com',
+        'password' => 'password',
+    ];
+
+    if (Auth::attempt($credentials)) {
+        return 'LOGIN BERHASIL';
+    }
+
+    return 'LOGIN GAGAL';
 });
