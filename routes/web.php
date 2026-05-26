3091a3f36cef8f1dc3531c1ad2792cd62d25f960
12rekayasa2-rpl <12rekayasa2@gmail.com>
2026-05-26 15:40:56 +0800

    add clear cache route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/clear', function () {
+    Artisan::call('optimize:clear');
+    return 'cache cleared';
 });
